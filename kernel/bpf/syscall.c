a5113c574b1faa00abf440d54eb1994add3ae31a
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-01-17 19:13:28 -0800

    BACKPORT: bpf: offload: report device information about offloaded maps
    Tell user space about device on which the map was created.
    Unfortunate reality of user ABI makes sharing this code
    with program offload difficult but the information is the
    same.
    
    Change-Id: I4828cc703defb0d2b2ff3fab0d9e28a318f31505
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1868,6 +1868,12 @@ static int bpf_map_get_info_by_fd(struct bpf_map *map,
 	info.max_entries = map->max_entries;
 	info.map_flags = map->map_flags;
 
+	if (bpf_map_is_dev_bound(map)) {
+		err = bpf_map_offload_info_fill(&info, map);
+		if (err)
+			return err;
+	}
+
 	if (copy_to_user(uinfo, &info, info_len) ||
 	    put_user(info_len, &uattr->info.info_len))
 		return -EFAULT;
