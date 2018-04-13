c5e2ffe13f86865a9556ae2f330ad2396ad646b2
Andi Shyti <andi.shyti@samsung.com>
2018-04-13 09:48:29 -0400

    UPSTREAM: media: rc: ir-spi: update Andi's e-mail
    Because I will be leaving Samsung soon, update my e-mail to the
    etezian.org mail.
    
    Cc: Andi Shyti <andi@etezian.org>
    Change-Id: If0e50cf2a90cc0d1131469d2d0c9b6f55d10b5ae
    Signed-off-by: Andi Shyti <andi.shyti@samsung.com>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- drivers/media/rc/ir-spi.c
+++ drivers/media/rc/ir-spi.c
@@ -2,7 +2,7 @@
 // SPI driven IR LED device driver
 //
 // Copyright (c) 2016 Samsung Electronics Co., Ltd.
-// Copyright (c) Andi Shyti <andi.shyti@samsung.com>
+// Copyright (c) Andi Shyti <andi@etezian.org>
 
 #include <linux/delay.h>
 #include <linux/kthread.h>
@@ -279,6 +279,6 @@ static struct spi_driver ir_spi_driver = {
 
 module_spi_driver(ir_spi_driver);
 
-MODULE_AUTHOR("Andi Shyti <andi.shyti@samsung.com>");
+MODULE_AUTHOR("Andi Shyti <andi@etezian.org>");
 MODULE_DESCRIPTION("SPI IR LED");
 MODULE_LICENSE("GPL v2");
