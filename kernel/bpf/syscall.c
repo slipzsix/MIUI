e0ca411a665520a40a60047ed93b57660428e1d7
Jiri Olsa <jolsa@kernel.org>
2018-04-25 19:41:06 +0200

    UPSTREAM: bpf: Add gpl_compatible flag to struct bpf_prog_info
    Adding gpl_compatible flag to struct bpf_prog_info
    so it can be dumped via bpf_prog_get_info_by_fd and
    displayed via bpftool progs dump.
    
    Alexei noticed 4-byte hole in struct bpf_prog_info,
    so we put the u32 flags field in there, and we can
    keep adding bit fields in there without breaking
    user space.
    
    Change-Id: Ib12a4e3c5f52de241a99f745990358caa3967ced
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1984,6 +1984,10 @@ static int bpf_prog_get_info_by_fd(struct bpf_prog *prog,
 
 	info.type = prog->type;
 	info.id = prog->aux->id;
+	info.load_time = prog->aux->load_time;
+	info.created_by_uid = from_kuid_munged(current_user_ns(),
+					       prog->aux->user->uid);
+	info.gpl_compatible = prog->gpl_compatible;
 
 	memcpy(info.tag, prog->tag, sizeof(prog->tag));
 	memcpy(info.name, prog->aux->name, sizeof(prog->aux->name));
