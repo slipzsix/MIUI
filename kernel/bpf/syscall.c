9da8f2817bc3137fb6f8c73c62f2b62eab6a11b3
Jason A. Donenfeld <Jason@zx2c4.com>
2019-06-21 22:32:48 +0200

    BACKPORT: timekeeping: Use proper clock specifier names in functions
    This makes boot uniformly boottime and tai uniformly clocktai, to
    address the remaining oversights.
    
    Change-Id: I3463b9045bddeba00d6f9fcf78d63008459c1b9a
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lkml.kernel.org/r/20190621203249.3909-2-Jason@zx2c4.com
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1740,6 +1740,11 @@ static int bpf_prog_load(union bpf_attr *attr, union bpf_attr __user *uattr)
 	if (err < 0)
 		goto free_prog;
 
+	prog->aux->load_time = ktime_get_boottime_ns();
+	err = bpf_obj_name_cpy(prog->aux->name, attr->prog_name);
+	if (err)
+		goto free_prog;
+
 	/* run eBPF verifier */
 	err = bpf_check(&prog, attr, uattr);
 	if (err < 0)
