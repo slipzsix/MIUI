96a5ace15e7eb5240e856dee384ddbccc8c53498
Yaroslav Furman <yaro330@gmail.com>
2020-01-04 01:37:19 +0300

    msm: vidc: Disable all debugging
    I don't give a fuck about your errors.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -19,22 +19,20 @@
 #include "msm_vidc_debug.h"
 #include "vidc_hfi_api.h"
 
+int msm_vidc_debug = 0;
+EXPORT_SYMBOL(msm_vidc_debug);
+
+int msm_vidc_debug_out = 0;
+EXPORT_SYMBOL(msm_vidc_debug_out);
+
 /* 0x18 = HFI_DEBUG_MSG_FATAL | HFI_DEBUG_MSG_ERROR */
-int msm_vidc_fw_debug = 0x18;
+int msm_vidc_fw_debug = 0;
+int msm_vidc_fw_debug_mode = 0;
 int msm_vidc_fw_low_power_mode = 1;
 bool msm_vidc_fw_coverage = !true;
 bool msm_vidc_thermal_mitigation_disabled = !true;
 int msm_vidc_clock_voting = !1;
 bool msm_vidc_syscache_disable = !true;
-
-#ifdef CONFIG_DEBUG_KERNEL
-int msm_vidc_debug = VIDC_ERR | VIDC_WARN | VIDC_FW;
-EXPORT_SYMBOL(msm_vidc_debug);
-
-int msm_vidc_debug_out = VIDC_OUT_PRINTK;
-EXPORT_SYMBOL(msm_vidc_debug_out);
-
-int msm_vidc_fw_debug_mode = 1;
 
 #define MAX_DBG_BUF_SIZE 4096
 
