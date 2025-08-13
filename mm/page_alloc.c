8d080121b543c3210580d4027fc64fa302a44a3b
slipzsix <slipzsix@gmail.com>
2025-08-13 13:11:41 +0700

    Revert: Change: int min_free_kbytes page_alloc.c

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -319,7 +319,7 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 32768;
+int min_free_kbytes = 1024;
 int user_min_free_kbytes = -1;
 int watermark_scale_factor = 25;
 
