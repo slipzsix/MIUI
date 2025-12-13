33dba597fe430863e6bd6a757f619c27b0ebb0c7
slipzsix <slipzsix@gmail.com>
2025-12-13 17:07:46 +0700

    sched/core_ctl: drop tracing and silence logging
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>
    Signed-off-by: slipzsix <slipzsix@gmail.com>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -23,7 +23,6 @@
 #include <uapi/linux/sched/types.h>
 #include <linux/sched/core_ctl.h>
 
-#include <trace/events/sched.h>
 #include "sched.h"
 #include "walt.h"
 
@@ -655,11 +654,6 @@ static void update_running_avg(void)
 		cluster->max_nr = compute_cluster_max_nr(index);
 		cluster->nr_prev_assist = prev_cluster_nr_need_assist(index);
 
-		trace_core_ctl_update_nr_need(cluster->first_cpu, nr_need,
-					prev_misfit_need,
-					cluster->nrrun, cluster->max_nr,
-					cluster->nr_prev_assist);
-
 		big_avg += cluster_real_big_tasks(index);
 	}
 	spin_unlock_irqrestore(&state_lock, flags);
@@ -754,8 +748,6 @@ static bool eval_need(struct cluster_data *cluster)
 			else if (c->busy < cluster->busy_down_thres[thres_idx])
 				c->is_busy = false;
 
-			trace_core_ctl_set_busy(c->cpu, c->busy, old_is_busy,
-						c->is_busy);
 			need_cpus += c->is_busy;
 		}
 		need_cpus = apply_task_need(cluster, need_cpus);
@@ -788,8 +780,6 @@ static bool eval_need(struct cluster_data *cluster)
 		cluster->need_ts = now;
 		cluster->need_cpus = new_need;
 	}
-	trace_core_ctl_eval_need(cluster->first_cpu, last_need, new_need,
-				 ret && need_flag);
 	spin_unlock_irqrestore(&state_lock, flags);
 
 	return ret && need_flag;
@@ -849,9 +839,6 @@ int core_ctl_set_boost(bool boost)
 		for_each_cluster(cluster, index)
 			apply_need(cluster);
 	}
-
-	if (cluster)
-		trace_core_ctl_set_boost(cluster->boost, ret);
 
 	return ret;
 }
@@ -1230,8 +1217,6 @@ static int cluster_init(const struct cpumask *mask)
 	if (!dev)
 		return -ENODEV;
 
-	pr_info("Creating CPU group %d\n", first_cpu);
-
 	if (num_clusters == MAX_CLUSTERS) {
 		pr_err("Unsupported number of clusters. Only %u supported\n",
 								MAX_CLUSTERS);
@@ -1260,8 +1245,6 @@ static int cluster_init(const struct cpumask *mask)
 	spin_lock_init(&cluster->pending_lock);
 
 	for_each_cpu(cpu, mask) {
-		pr_info("Init CPU%u state\n", cpu);
-
 		state = &per_cpu(cpu_state, cpu);
 		state->cluster = cluster;
 		state->cpu = cpu;
