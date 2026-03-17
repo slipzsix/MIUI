5d6c16aebb821c11de3fea03494dd8ae3c8f2a3a
slipzSIX <slipzsix@gmail.com>
2026-03-17 16:03:35 +0700

    power: supply: Add bypass charging enabled control and safety checks
    Add support for POWER_SUPPLY_PROP_CHARGING_ENABLED to allow userspace
    to control battery charging state through the power_supply framework.
    
    Charging state is controlled via chg_disable_votable using USER_VOTER.
    
    Additionally add safety validation and fallback values for charger
    parameters to avoid invalid configurations that may affect charging
    stability.
    
    This enables better compatibility with Android charging control
    mechanisms and battery management tools.
    
    This is squash commit bypass charging for sweet.
    
    Co-authored-by: Rve27 <rve27github@gmail.com>
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2104,6 +2104,7 @@ static int smb5_init_dc_psy(struct smb5 *chip)
  * BATT PSY REGISTRATION *
  *************************/
 static enum power_supply_property smb5_batt_props[] = {
+    POWER_SUPPLY_PROP_CHARGING_ENABLED,
 	POWER_SUPPLY_PROP_INPUT_SUSPEND,
 	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_HEALTH,
@@ -2166,6 +2167,9 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_PRESENT:
 		rc = smblib_get_prop_batt_present(chg, val);
+		break;
+	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+		val->intval = !get_effective_result(chg->chg_disable_votable);
 		break;
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 		rc = smblib_get_prop_input_suspend(chg, val);
@@ -2350,6 +2354,9 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_STATUS:
 		rc = smblib_set_prop_batt_status(chg, val);
 		break;
+	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+		vote(chg->chg_disable_votable, USER_VOTER, !!!val->intval, 0);
+		break;
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 		rc = smblib_set_prop_input_suspend(chg, val);
 		break;
@@ -2487,6 +2494,7 @@ static int smb5_batt_prop_is_writeable(struct power_supply *psy,
 {
 	switch (psp) {
 	case POWER_SUPPLY_PROP_STATUS:
+	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
 	case POWER_SUPPLY_PROP_CAPACITY:
