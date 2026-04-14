6a6283f06cd55dcd1da10cb0e028e150e2d0ab9b
slipzSIX <slipzsix@gmail.com>
2026-04-14 13:28:32 +0700

    IMPORT: sweet merge misc XIAOMI DRM, DT2W, Input ,Display changes
    - Import Xiaomi DRM (AOD/HBM fixes)
    - Add DT2W support for TDDI and GT9896
    - Integrate FPC TEE fingerprint drivers with necessary fixes and configs
    - input goodix suppres spam logs
    
    Co-authored-by: Zyusz <zyusz6164@gmail.com>
    Co-authored-by: MDonip <muhammaddoni007@gmail.com>
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- drivers/gpu/drm/drm_ioctl.c
+++ drivers/gpu/drm/drm_ioctl.c
@@ -2,8 +2,8 @@
  * Created: Fri Jan  8 09:01:26 1999 by faith@valinux.com
  *
  * Copyright 1999 Precision Insight, Inc., Cedar Park, Texas.
- * Copyright 2000 VA Linux Systems, Inc., Sunnyvale, California.
  * Copyright (C) 2021 XiaoMi, Inc.
+ * Copyright 2000 VA Linux Systems, Inc., Sunnyvale, California.
  * All Rights Reserved.
  *
  * Author Rickard E. (Rik) Faith <faith@valinux.com>
@@ -546,7 +546,7 @@ int drm_ioctl_permit(u32 flags, struct drm_file *file_priv)
 		return -EACCES;
 
 	/* MASTER is only for master or control clients */
-	if (unlikely((flags & DRM_MASTER) && 
+	if (unlikely((flags & DRM_MASTER) &&
 		     !drm_is_current_master(file_priv) &&
 		     !drm_is_control_client(file_priv))) {
 		if (!drm_master_filter(task->comm)) {
