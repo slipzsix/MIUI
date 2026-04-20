af8ee88ebebb057a93d24b7bf3f72dc4616025d9
slipzSIX <slipzsix@gmail.com>
2026-04-20 16:56:02 +0700

    drivers: msm: don't copy fence names by default
    Same concept as here: kerneltoast/android_kernel_google_wahoo@fe23bc0
    Extended version that covers more cases.
    
    Co-authored-by: Yaroslav Furman <yaro330@gmail.com>
    [kdrag0n: Fixed compile error in Adreno driver when debugfs is enabled]
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- drivers/gpu/msm/kgsl_drawobj.c
+++ drivers/gpu/msm/kgsl_drawobj.c
@@ -46,6 +46,19 @@ static struct kmem_cache *sparseobjs_cache;
 static struct kmem_cache *drawobj_sparse_cache;
 static struct kmem_cache *drawobj_sync_cache;
 static struct kmem_cache *drawobj_cmd_cache;
+#ifdef CONFIG_FENCE_DEBUG
+static void free_fence_names(struct kgsl_drawobj_sync *syncobj)
+{
+	unsigned int i;
+
+	for (i = 0; i < syncobj->numsyncs; i++) {
+		struct kgsl_drawobj_sync_event *event = &syncobj->synclist[i];
+
+		if (event->type == KGSL_CMD_SYNCPOINT_TYPE_FENCE)
+			kfree(event->info.fences);
+	}
+}
+#endif
 
 void kgsl_drawobj_destroy_object(struct kref *kref)
 {
@@ -58,6 +71,9 @@ void kgsl_drawobj_destroy_object(struct kref *kref)
 	switch (drawobj->type) {
 	case SYNCOBJ_TYPE:
 		syncobj = SYNCOBJ(drawobj);
+#ifdef CONFIG_FENCE_DEBUG
+		free_fence_names(syncobj);
+#endif
 		kfree(syncobj->synclist);
 		kmem_cache_free(drawobj_sync_cache, syncobj);
 		break;
@@ -98,6 +114,14 @@ void kgsl_dump_syncpoints(struct kgsl_device *device,
 			break;
 		}
 		case KGSL_CMD_SYNCPOINT_TYPE_FENCE: {
+#ifdef CONFIG_FENCE_DEBUG
+			int j;
+			struct event_fence_info *info = &event->info;
+
+			for (j = 0; j < info->num_fences; j++)
+				dev_err(device->dev, "[%d]  fence: %s\n",
+					i, info->fences[j].name);
+#endif
 			break;
 		}
 		}
@@ -145,6 +169,14 @@ static void syncobj_timer(unsigned long data)
 				i, event->context->id, event->timestamp);
 			break;
 		case KGSL_CMD_SYNCPOINT_TYPE_FENCE: {
+#ifdef CONFIG_FENCE_DEBUG
+			int j;
+			struct event_fence_info *info = &event->info;
+
+			for (j = 0; j < info->num_fences; j++)
+				dev_err(device->dev, "       [%d] FENCE %s\n",
+					i, info->fences[j].name);
+#endif
 			break;
 		}
 		}
@@ -330,6 +362,13 @@ EXPORT_SYMBOL(kgsl_drawobj_destroy);
 static bool drawobj_sync_fence_func(void *priv)
 {
 	struct kgsl_drawobj_sync_event *event = priv;
+#ifdef CONFIG_FENCE_DEBUG
+	int i;
+
+	for (i = 0; i < event->info.num_fences; i++)
+		trace_syncpoint_fence_expire(event->syncobj,
+			event->info.fences[i].name);
+#endif
 
 	/*
 	 * Only call kgsl_drawobj_put() if it's not marked for cancellation
@@ -391,6 +430,11 @@ static int drawobj_add_sync_fence(struct kgsl_device *device,
 
 		return ret;
 	}
+
+#ifdef CONFIG_FENCE_DEBUG
+	for (i = 0; i < event->info.num_fences; i++)
+		trace_syncpoint_fence(syncobj, event->info.fences[i].name);
+#endif
 
 	return 0;
 }
