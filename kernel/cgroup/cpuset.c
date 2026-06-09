85a8fe60a52f66ed7d05038d81083786921cddd6
slipzSIX <slipzsix@gmail.com>
2026-06-09 13:43:22 +0700

    cpuset: #superryzens balance perf
    Signed-off-by: slipzsix <slipzsix@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1805,12 +1805,12 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 #ifdef CONFIG_CPUSETS_ASSIST
 	static struct cs_target cs_targets[] = {
 		/* Little-only cpusets go first */
-		{ "foreground",		"0-5,7" },
-		{ "background",		"4-5" },
-		{ "system-background",	"2-5" },
-		{ "restricted",		"2-5" },
+		{ "foreground",		"0-5,6" },
+		{ "background",		"0-3" },
+		{ "system-background",	"0-3" },
+		{ "restricted",		"0-1" },
 		{ "top-app",		"0-7" },
-		{ "camera-daemon",	"0-7" },
+		{ "camera-daemon",	"0-3,6" },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
