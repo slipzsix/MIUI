47804437780f4eefd4d97747edd50066673f1f98
slipzSIX <slipzsix@gmail.com>
2026-08-07 21:12:52 +0700

    [BACKPORT]: Implement susfs v2.2
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1183,6 +1183,10 @@ static int override_release(char __user *release, size_t len)
 }
 
 extern bool is_legacy_ebpf;
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1190,16 +1194,19 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!is_legacy_ebpf) {
-		if (!strncmp(current->comm, "bpfloader", 9) ||
-			!strncmp(current->comm, "netbpfload", 10) ||
-			!strncmp(current->comm, "netd", 4)) {
-			if (current_uid().val == 0) {
-				strcpy(tmp.release, "5.10.239");
-				pr_debug("fake uname: %s/%d release=%s\n",
-					current->comm, current->pid, tmp.release);
-			}
+		if (current_uid().val == 0 &&
+		   (!strncmp(current->comm, "bpfloader", 9) ||
+		    !strncmp(current->comm, "netbpfload", 10) ||
+		    !strncmp(current->comm, "uprobestatsbpfload", 18) ||
+		    !strncmp(current->comm, "netd", 4))) {
+			strcpy(tmp.release, "5.10.239");
+			pr_debug("fake uname: %s/%d release=%s\n",
+				current->comm, current->pid, tmp.release);
 		}
 	}
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
