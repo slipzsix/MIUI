20aaaf645642ec47246a52440c97832103aec175
slipzSIX <slipzsix@gmail.com>
2026-08-22 20:26:44 +0700

    NoMount: systemless modifications
    https://github.com/maxsteeel/nomount/tree/29652753260c6f1cd558df64730d3314c75bf076/kernel
    
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- fs/readdir.c
+++ fs/readdir.c
@@ -23,6 +23,10 @@
 
 #include <linux/uaccess.h>
 
+#ifdef CONFIG_NOMOUNT
+extern int nomount_handle_iterate_dir(struct file *file, struct dir_context *ctx);
+#endif
+
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 #include <linux/susfs_def.h>
 extern bool susfs_is_inode_sus_path(struct inode *inode);
@@ -53,10 +57,14 @@ int iterate_dir(struct file *file, struct dir_context *ctx)
 	res = -ENOENT;
 	if (!IS_DEADDIR(inode)) {
 		ctx->pos = file->f_pos;
+#ifdef CONFIG_NOMOUNT
+		res = nomount_handle_iterate_dir(file, ctx);
+#else
 		if (shared)
 			res = file->f_op->iterate_shared(file, ctx);
 		else
 			res = file->f_op->iterate(file, ctx);
+#endif
 		file->f_pos = ctx->pos;
 		fsnotify_access(file);
 		file_accessed(file);
