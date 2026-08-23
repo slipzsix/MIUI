4ef8eb9d17953d0b3ba8f71d783ec7e737e9a1c2
slipzSIX <slipzsix@gmail.com>
2026-08-24 05:58:42 +0700

    simple_lmk: Relax memory reclamation scan interval to reduce reload apps
    This relaxes the scan_and_kill loop frequency to prevent aggressive
    background process termination, reducing app reloads and improving
    multitasking usability.
    
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -204,6 +204,8 @@ static void set_task_rt_prio(struct task_struct *tsk, int priority)
 	sched_setscheduler_nocheck(tsk, SCHED_RR, &rt_prio);
 }
 
+#define SLEEP_DURATION_MS 40
+
 static void scan_and_kill(void)
 {
 	int i, nr_to_kill = 0;
@@ -300,7 +302,7 @@ static void scan_and_kill(void)
 	if (!wait_for_completion_timeout(&reclaim_done, RECLAIM_EXPIRES))
 		pr_info("Timeout hit waiting for victims to die, proceeding\n");
 	else
-		msleep(28);
+		msleep(SLEEP_DURATION_MS);
 
 	/* Clean up for future reclaims but let the reaper thread keep going */
 	write_lock(&mm_free_lock);
