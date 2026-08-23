b8d89f985077ecd3dd7c8afaae94fe3c6ffcceca
slipzSIX <slipzsix@gmail.com>
2026-08-23 22:35:31 +0700

    Revert "simple_lmk: expose simple_lmk_trigger()"
    This reverts commit 0c6692e66e75d9371e64fd88b55146d99a4db58a.

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -452,19 +452,14 @@ void simple_lmk_mm_freed(struct mm_struct *mm)
 	read_unlock(&mm_free_lock);
 }
 
-void simple_lmk_trigger(void)
-{
-	atomic_set(&needs_reclaim, 1);
-	smp_mb__after_atomic();
-	if (waitqueue_active(&oom_waitq))
-		wake_up(&oom_waitq);
-}
-
 static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
 				    unsigned long pressure, void *data)
 {
 	if (pressure >= 98) {
-		simple_lmk_trigger();
+		atomic_set(&needs_reclaim, 1);
+		smp_mb__after_atomic();
+		if (waitqueue_active(&oom_waitq))
+			wake_up(&oom_waitq);
 	}
 
 	return NOTIFY_OK;
