730b15b19a6e13c93e1fb3c72029addf0a613bb4
slipzSIX <slipzsix@gmail.com>
2026-09-13 20:50:49 +0700

    Revert "selinux: cleanup printk avoid dmesg logging and disable audit"
    This reverts commit fca7b40828161b0b31c39386acf3572f08b71568.
    
    Signed-off-by: slipzSIX <slipzsix@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3177,7 +3177,6 @@ static noinline int audit_inode_permission(struct inode *inode,
 					   int result,
 					   unsigned flags)
 {
-#ifdef CONFIG_AUDIT
 	struct common_audit_data ad;
 	struct inode_security_struct *isec = selinux_inode(inode);
 	int rc;
@@ -3190,7 +3189,6 @@ static noinline int audit_inode_permission(struct inode *inode,
 			    audited, denied, result, &ad, flags);
 	if (rc)
 		return rc;
-#endif
 	return 0;
 }
 
@@ -6991,11 +6989,6 @@ static struct security_hook_list selinux_hooks[] __lsm_ro_after_init = {
 
 static __init int selinux_init(void)
 {
-	if (!selinux_enabled_boot) {
-		pr_info("SELinux:  Disabled at boot.\n");
-		return 0;
-	}
-
 	pr_info("SELinux:  Initializing.\n");
 
 	memset(&selinux_state, 0, sizeof(selinux_state));
